4415782eb6cc84521b55445d1aa3678d19d5ecf4
wojceichciejka <wojciech.ciejka@icloud.com>
2020-11-21 15:04:04 +0100

    Redis Service added

---
+++ src/main/java/com/gymapplicationbackend/gymapplicationbackendapi/service/RedisService.java
@@ -0,0 +1,110 @@
+package com.gymapplicationbackend.gymapplicationbackendapi.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gymapplicationbackend.gymapplicationbackendapi.config.SessionUser;
+import com.gymapplicationbackend.gymapplicationbackendapi.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisService {
+
+    private static final String KEY_USER = "USERS";
+    private static final String KEY_TOKEN = "TOKEN_";
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    private RedisTemplate template;
+
+    public synchronized List<String> getKeys(final String pattern) {
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        Set<String> redisKeys = template.keys(pattern);
+        // Store the keys in a List
+        List<String> keysList = new ArrayList<>();
+        Iterator<String> it = redisKeys.iterator();
+        while (it.hasNext()) {
+            String data = it.next();
+            keysList.add(data);
+        }
+        return keysList;
+    }
+
+    public boolean addToken(SessionUser sessionUser) {
+        try{
+            String key = KEY_TOKEN + sessionUser.getUsername();
+            template.opsForHash().put(key, sessionUser.getUsername(), sessionUser);
+            template.expire(key, 1, TimeUnit.HOURS);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User getUser(String username) {
+        User user;
+        user = (User) template.opsForHash().get(KEY_USER, username);
+        return user;
+    }
+
+    public synchronized Object getValue(final String key) {
+
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template.opsForValue().get(key);
+    }
+
+    public synchronized Object getValue(final String key, Class clazz) {
+        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+
+        Object obj = template.opsForValue().get(key);
+        return mapper.convertValue(obj, clazz);
+    }
+
+    public void setValue(final String key, final Object value) {
+        setValue(key, value, TimeUnit.HOURS, 5, false);
+    }
+
+    public void setValue(final String key, final Object value, boolean marshal) {
+        if (marshal) {
+            template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+            template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        } else {
+            template.setHashValueSerializer(new StringRedisSerializer());
+            template.setValueSerializer(new StringRedisSerializer());
+        }
+        template.opsForValue().set(key, value);
+        // set a expire for a message
+        template.expire(key, 5, TimeUnit.MINUTES);
+    }
+
+    public void setValue(final String key, final Object value, TimeUnit unit, long timeout) {
+        setValue(key, value, unit, timeout, false);
+    }
+
+    public void setValue(final String key, final Object value, TimeUnit unit, long timeout, boolean marshal) {
+        if (marshal) {
+            template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+            template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        } else {
+            template.setHashValueSerializer(new StringRedisSerializer());
+            template.setValueSerializer(new StringRedisSerializer());
+        }
+        template.opsForValue().set(key, value);
+        // set a expire for a message
+        template.expire(key, timeout, unit);
+    }
+
+}
+
