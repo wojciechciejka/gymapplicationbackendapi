a852e30091f81fb8346d146555b6a2258aabd6e4
wojceichciejka <wojciech.ciejka@icloud.com>
2020-11-22 15:12:34 +0100

    NewUser class added

---
+++ src/main/java/com/gymapplicationbackend/gymapplicationbackendapi/model/NewUser.java
@@ -0,0 +1,15 @@
+package com.gymapplicationbackend.gymapplicationbackendapi.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class NewUser implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String emailId;
+    private String password;
+    private int age;
+}
